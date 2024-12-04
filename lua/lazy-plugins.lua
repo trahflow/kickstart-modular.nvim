a89d05977bd90a8de423a7aa14933c2a73de1112
Wolfhart Feldmeier <wolfhart.feldmeier@protonmail.com>
2024-12-04 19:15:33 +0100

    add neotree plugin

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -43,6 +43,8 @@ require('lazy').setup({
 
   require 'kickstart/plugins/treesitter',
 
+  require 'kickstart.plugins.neo-tree',
+
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
@@ -56,7 +58,6 @@ require('lazy').setup({
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
