c7f8efc2cb4df6bfe71827140b838fc9397a52a2
Wolfhart Feldmeier <wolfhart.feldmeier@blueyonder.com>
2025-03-13 10:14:11 +0100

    add treesitter-context plugin

--- lua/kickstart/plugins/treesitter.lua
+++ lua/kickstart/plugins/treesitter.lua
@@ -25,5 +25,6 @@ return {
     --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
+  { 'nvim-treesitter/nvim-treesitter-context', config = true },
 }
 -- vim: ts=2 sts=2 sw=2 et
