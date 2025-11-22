9181a11ed545347e4be391870d77f173842fcffc
Wolfhart Feldmeier <wolfhart.feldmeier@protonmail.com>
2025-11-22 14:57:04 +0100

    mcphub: Load optionally based on environment variable

--- lua/custom/plugins/mcphub.lua
+++ lua/custom/plugins/mcphub.lua
@@ -3,6 +3,9 @@ return {
   dependencies = {
     'nvim-lua/plenary.nvim',
   },
+  enabled = function()
+    return os.getenv 'NVIM_AI_PROVIDER' ~= nil
+  end,
   build = 'npm install -g mcp-hub@latest', -- Installs `mcp-hub` node binary globally
   config = function()
     require('mcphub').setup()
